7fbbd702b029c282b73b9ec02624fcab393e28a3
Hubert Łępicki <hubert.lepicki@amberbit.com>
2017-03-06 19:09:53 +0100

    remove index listing

--- lib/srv.rb
+++ lib/srv.rb
@@ -8,29 +8,13 @@ module Srv
       root = Dir.pwd
       puts ">>> serving contents of directory: #{root}"
 
-      @rack_server = Rack::Server.new(:app => DirectoryWithIndex.new(root),
+      @rack_server = Rack::Server.new(:app => Rack::Directory.new(root),
                                       :Port => port,
                                       :AccessLog => [])
     end
 
     def run!
       @rack_server.start
-    end
-  end
-
-  class DirectoryWithIndex < Rack::Directory
-    def list_directory(path_info, path, script_name)
-      index, = Dir.glob(File.join(path, "index.htm*"))
-      if index
-        index_path_info = path_info
-        index_path_info += "/" unless path_info[-1] == "/"
-        index_path_info += File.basename index
-        altered_env = @env.dup
-        altered_env["PATH_INFO"] = index_path_info
-        @app.call(altered_env)
-      else
-        super(path_info, path, script_name)
-      end
     end
   end
 end
